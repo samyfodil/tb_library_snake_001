213a027ee2fe2ca1179a752e2223a574f6f76224
samyfodil <fodil.samy@gmail.com>
2023-05-15 17:30:24 -0500

    look

--- server.go
+++ server.go
@@ -82,7 +82,7 @@ func move(e event.Event) uint32 {
 		return 1
 	}
 
-	response := domove5(state)
+	response := domove4(state)
 	//response := MoveStrategyX{NewMoveHelper(16)}.GetMove(state)
 
 	h.Headers().Set("Content-Type", "application/json")
